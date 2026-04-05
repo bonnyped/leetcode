913dc61c7df9a777fe95c2e4aaf834d580501568
bonnyped <bonnyped@gmail.com>
2026-04-05 12:45:53 +0600

    contains duplicate + tests = done

--- 01_HashMap/Contains_Duplicate/main.go
+++ 01_HashMap/Contains_Duplicate/main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"testing"
-)
-
-func TestContainsDuplicate(t *testing.T) {
-	tests := []struct {
-		nums     []int
-		expected bool
-	}{
-		{[]int{1, 2, 3, 1}, true},
-		{[]int{1, 2, 3, 4}, false},
-		{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
-		{[]int{}, false},
-		{[]int{0}, false},
-	}
-
-	for _, tt := range tests {
-		result := containsDuplicate(tt.nums)
-
-		if result != tt.expected {
-			t.Errorf("got %t, want %t", result, tt.expected)
-		}
-	}
-}
-
 func containsDuplicate(nums []int) bool {
 	accumulator := make(map[int]struct{})
 
