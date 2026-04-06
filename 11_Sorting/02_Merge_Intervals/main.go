01259aa598076494f77a77220d3d1a7afb4aec4e
bonnyped <bonnyped@gmail.com>
2026-04-06 16:50:42 +0600

    merge intervals v1 - done

--- 11_Sorting/02_Merge_Intervals/main.go
+++ 11_Sorting/02_Merge_Intervals/main.go
@@ -1,5 +1,41 @@
 package main
 
-func merge(intervals [][]int) [][]int {
+import (
+	"fmt"
+	"sort"
+)
 
+func merge(intervals [][]int) [][]int {
+	result := [][]int{}
+	min, max := 0, 0
+	length := len(intervals)
+
+	if length <= 1 {
+		return intervals
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	for i := 0; i < length; i++ {
+		min = intervals[i][0]
+		max = intervals[i][1]
+		for j := i + 1; j < length; j++ {
+			if intervals[j][0] <= max {
+				if intervals[j][1] > max {
+					max = intervals[j][1]
+				}
+				i++
+			} else {
+				break
+			}
+		}
+
+		result = append(result, []int{min, max})
+	}
+
+	fmt.Println(result)
+
+	return result
 }
--- 11_Sorting/02_Merge_Intervals/main_test.go
+++ 11_Sorting/02_Merge_Intervals/main_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSolution(t *testing.T) {
+	tests := []struct {
+		intervals [][]int
+		expected  [][]int
+	}{
+		{[][]int{{15, 18}, {1, 3}, {2, 6}, {8, 10}}, [][]int{{1, 6}, {8, 10}, {15, 18}}},
+		{[][]int{{1, 4}, {4, 5}}, [][]int{{1, 5}}},
+		{[][]int{{4, 7}, {1, 4}}, [][]int{{1, 7}}},
+		{[][]int{{}}, [][]int{{}}},
+		{[][]int{{1, 4}, {2, 3}}, [][]int{{1, 4}}},
+	}
 
+	for _, tt := range tests {
+		result := merge(tt.intervals)
+
+		if !reflect.DeepEqual(result, tt.expected) {
+			t.Errorf("got %v, want %v", result, tt.expected)
+		}
+	}
 }
