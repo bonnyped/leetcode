15ec621ee4a20149eb9520f00d372f62b525b213
bonnyped <bonnyped@gmail.com>
2026-04-11 18:26:36 +0600

    minor changes

--- 01_HashMap/Group_Anagrams/main.go
+++ 01_HashMap/Group_Anagrams/main.go
@@ -38,50 +38,50 @@ func sortString(str string) string {
 }
 
 // первое решение
-import "maps"
+// import "maps"
 
-type word struct {
-	dic  map[byte]int
-	strs []string
-}
+// type word struct {
+// 	dic  map[byte]int
+// 	strs []string
+// }
 
-func groupAnagrams(strs []string) [][]string {
-	result := make([]word, 0)
+// func groupAnagrams(strs []string) [][]string {
+// 	result := make([]word, 0)
 
-	for _, str := range strs {
-		dic := make(map[byte]int)
-		for j := 0; j < len(str); j++ {
-			dic[str[j]]++
-		}
+// 	for _, str := range strs {
+// 		dic := make(map[byte]int)
+// 		for j := 0; j < len(str); j++ {
+// 			dic[str[j]]++
+// 		}
 
-		w := dictionaryExist(result, dic)
+// 		w := dictionaryExist(result, dic)
 
-		if w == nil {
-			result = append(result, word{dic, []string{str}})
-		} else {
-			w.strs = append(w.strs, str)
-		}
+// 		if w == nil {
+// 			result = append(result, word{dic, []string{str}})
+// 		} else {
+// 			w.strs = append(w.strs, str)
+// 		}
 
-	}
+// 	}
 
-	return mergeResults(result)
-}
+// 	return mergeResults(result)
+// }
 
-func mergeResults(words []word) [][]string {
-	result := make([][]string, 0)
+// func mergeResults(words []word) [][]string {
+// 	result := make([][]string, 0)
 
-	for _, v := range words {
-		result = append(result, v.strs)
-	}
+// 	for _, v := range words {
+// 		result = append(result, v.strs)
+// 	}
 
-	return result
-}
+// 	return result
+// }
 
-func dictionaryExist(result []word, dic map[byte]int) *word {
-	for i := range result {
-		if maps.Equal(result[i].dic, dic) {
-			return &result[i]
-		}
-	}
-	return nil
-}
+// func dictionaryExist(result []word, dic map[byte]int) *word {
+// 	for i := range result {
+// 		if maps.Equal(result[i].dic, dic) {
+// 			return &result[i]
+// 		}
+// 	}
+// 	return nil
+// }
