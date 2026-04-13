b5e55c750ece9822c18fa9827deee7c229899f3c
bonnyped <bonnyped@gmail.com>
2026-04-13 11:39:37 +0600

    new version of contains dulicate

--- 01_HashMap/Contains_Duplicate/main.go
+++ 01_HashMap/Contains_Duplicate/main.go
@@ -1,14 +1,33 @@
 package main
 
-func containsDuplicate(nums []int) bool {
-	accumulator := make(map[int]struct{})
+func containsNearbyDuplicate(nums []int, k int) bool {
+	search := make(map[int]int)
 
-	for _, v := range nums {
-		if _, hasNum := accumulator[v]; hasNum {
+	for idx, num := range nums {
+		if i, found := search[num]; found && abs(i-idx) <= k {
 			return true
 		}
-		accumulator[v] = struct{}{}
+		search[num] = idx
 	}
-
 	return false
 }
+
+func abs(i int) int {
+	if i < 0 {
+		i *= -1
+	}
+	return i
+}
+
+// func containsDuplicate(nums []int) bool {
+// 	accumulator := make(map[int]struct{})
+
+// 	for _, v := range nums {
+// 		if _, hasNum := accumulator[v]; hasNum {
+// 			return true
+// 		}
+// 		accumulator[v] = struct{}{}
+// 	}
+
+// 	return false
+// }
--- 01_HashMap/Contains_Duplicate/main_test.go
+++ 01_HashMap/Contains_Duplicate/main_test.go
@@ -4,23 +4,48 @@ import (
 	"testing"
 )
 
-func TestContainsDuplicate(t *testing.T) {
+func TestContainsNearbyDuplicate(t *testing.T) {
 	tests := []struct {
-		nums     []int
+		input    []int
+		k        int
 		expected bool
 	}{
-		{[]int{1, 2, 3, 1}, true},
-		{[]int{1, 2, 3, 4}, false},
-		{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
-		{[]int{}, false},
-		{[]int{0}, false},
+		{[]int{1, 2, 3, 1}, 3, true},
+		{[]int{1, 0, 1, 1}, 1, true},
+		{[]int{-1, -10, 0, 1, 1}, 1, true},
+		{[]int{1, 2, 3, 1, 2, 3}, 2, false},
+		{[]int{1, 2, 3, 1, 2, 3}, 3, true},
+		{[]int{}, 0, false},
+		{[]int{1, 1}, 1, true},
+		{[]int{1, 1}, 0, false},
 	}
-
 	for _, tt := range tests {
-		result := containsDuplicate(tt.nums)
-
-		if result != tt.expected {
-			t.Errorf("got %t, want %t", result, tt.expected)
+		output := containsNearbyDuplicate(tt.input, tt.k)
+		if output != tt.expected {
+			t.Errorf("input %v: got %t, need %t", tt.input, output, tt.expected)
 		}
+
 	}
+
 }
+
+// func TestContainsDuplicate(t *testing.T) {
+// 	tests := []struct {
+// 		nums     []int
+// 		expected bool
+// 	}{
+// 		{[]int{1, 2, 3, 1}, true},
+// 		{[]int{1, 2, 3, 4}, false},
+// 		{[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
+// 		{[]int{}, false},
+// 		{[]int{0}, false},
+// 	}
+
+// 	for _, tt := range tests {
+// 		result := containsDuplicate(tt.nums)
+
+// 		if result != tt.expected {
+// 			t.Errorf("got %t, want %t", result, tt.expected)
+// 		}
+// 	}
+// }
