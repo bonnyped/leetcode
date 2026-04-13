822d78310e38e0503a55787a8fd30642633038f3
bonnyped <bonnyped@gmail.com>
2026-04-13 12:40:26 +0600

    is anagram - done

--- 01_HashMap/Valid_Anagram/main.go
+++ 01_HashMap/Valid_Anagram/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
 
-func main() {
-    fmt.Println("Hello, World!")
+	search := [26]int{}
+
+	for i := 0; i < len(s); i++ {
+		search[s[i]-'a']++
+		search[t[i]-'a']--
+	}
+
+	return search == [26]int{}
 }
